d5e2cdf5011e856503c856014d266fc98f2f58f5
Vincent Edwards <mister.man489@gmail.com>
2025-03-25 12:52:47 -0700

    Create a script to store default simulation parameters
    The dimensions were made larger compared to the simulation playground

---
+++ scripts/default_parameters.py
@@ -0,0 +1,11 @@
+parameters = {
+    "breed_time": 3,
+    "energy_gain": 4,
+    "breed_energy": 10,
+    "dims": (40, 60),
+    "initial_fish": 300,
+    "initial_sharks": 100,
+    "steps": 500,
+    "start_energy": 9,
+    "use_basic_setup": True,
+}
