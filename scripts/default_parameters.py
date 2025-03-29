8b4304ac43255c25a53e98fbddbfa09c8f5fd50a
Vincent Edwards <mister.man489@gmail.com>
2025-03-28 21:34:45 -0700

    Create functions to extract the needed parameters for functions

--- scripts/default_parameters.py
+++ scripts/default_parameters.py
@@ -9,3 +9,23 @@ parameters = {
     "start_energy": 9,
     "use_basic_setup": True,
 }
+
+def get_initialization_parameters(params):
+    """
+    Return a dictionary with just the parameters needed to initialize the game array.
+    """
+    result = {}
+    desired_keys = ["initial_fish", "initial_sharks", "breed_time", "breed_energy"]
+    for key in desired_keys:
+        result[key] = params[key]
+    return result
+
+def get_simulation_parameters(params):
+    """
+    Return a dictionary with just the parameters needed to run the simulation.
+    """
+    result = {}
+    desired_keys = ["steps", "breed_time", "energy_gain", "breed_energy", "start_energy"]
+    for key in desired_keys:
+        result[key] = params[key]
+    return result
